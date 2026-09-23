670338d340611d50ee2c0af2abb32fff4190fee8
Oluwaseyi-vibex <oluseyiwmwm@gmail.com>
2026-09-23 19:19:53 +0100

    feat: add ContactWithGlobe component and contact page

---
+++ app/contact/page.tsx
@@ -0,0 +1,12 @@
+import ContactWithGlobe from "@/components/ui/contact-globe";
+
+export default function ContactPage() {
+  return (
+    <div className="relative">
+      <ContactWithGlobe
+        title="Contact Us"
+        description="We are always looking for ways to improve our products and services. Contact us and let us know how we can help you."
+      />
+    </div>
+  );
+}
