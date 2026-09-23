006918bdc653f9b4817574aa838f6dfb48cb73bc
Oluwaseyi-vibex <oluseyiwmwm@gmail.com>
2026-09-23 20:05:12 +0100

    fix: navbar links scroll to sections on landing page

--- components/ui/slide-tabs.tsx
+++ components/ui/slide-tabs.tsx
@@ -34,8 +34,8 @@ export const SlideTabs = () => {
           <Image src="/logo.svg" alt="Inspire" width={80} height={80} className="rounded-xl" />
         </Link>
       </li>
-      {["Home", "About", "Gallery", "Support"].map((item) => (
-        <Tab key={item} setPosition={setPosition} href={item === "Home" ? "/" : `/${item.toLowerCase()}`}>{item}</Tab>
+      {["Home", "About", "Gallery", "Contact"].map((item) => (
+        <Tab key={item} setPosition={setPosition} href={item === "Home" ? "/" : `#${item.toLowerCase()}`}>{item}</Tab>
       ))}
 
 
