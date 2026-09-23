8c8186f0ca25884bab12a47249da81ff091faf11
Oluwaseyi-vibex <oluseyiwmwm@gmail.com>
2026-09-23 16:15:15 +0100

    feat: integrate slide-tabs into navbar

---
+++ components/ui/slide-tabs.tsx
@@ -0,0 +1,71 @@
+"use client";
+import { useRef, useState } from "react";
+import { motion } from "motion/react";
+
+export const SlideTabsExample = () => {
+  return (
+    <div className="bg-neutral-100 py-20 w-full min-h-screen">
+      <SlideTabs />
+    </div>
+  );
+};
+
+const SlideTabs = () => {
+  const [position, setPosition] = useState({
+    left: 0,
+    width: 0,
+    opacity: 0,
+  });
+
+  return (
+    <ul
+      onMouseLeave={() => {
+        setPosition((pv) => ({
+          ...pv,
+          opacity: 0,
+        }));
+      }}
+      className="relative mx-auto flex w-fit rounded-full border-2 border-black bg-white p-1"
+    >
+      <Tab setPosition={setPosition}>Home</Tab>
+      <Tab setPosition={setPosition}>About</Tab>
+      <Tab setPosition={setPosition}>Gallery</Tab>
+      <Tab setPosition={setPosition}>Contact</Tab>
+
+      <Cursor position={position} />
+    </ul>
+  );
+};
+
+const Tab = ({ children, setPosition }: { children: string; setPosition: any }) => {
+  const ref = useRef<HTMLDivElement>(null);
+
+  return (
+    <li
+      ref={ref}
+      onMouseEnter={() => {
+        if (!ref?.current) return;
+        const { width } = ref.current.getBoundingClientRect();
+        setPosition({
+          left: ref.current.offsetLeft,
+          width,
+          opacity: 1,
+        });
+      }}
+      className="relative z-10 block cursor-pointer px-3 py-1.5 text-xs uppercase text-white mix-blend-difference md:px-5 md:py-3 md:text-base"
+    >
+      {children}
+    </li>
+  );
+};
+
+const Cursor = ({ position }: { position: { left: number; width: number; opacity: number } }) => {
+  return (
+    <motion.li
+      animate={{
+        ...position,
+      }}
+      className="absolute z-0 h-7 rounded-full bg-black md:h-12"
+    />
+  );
+};
